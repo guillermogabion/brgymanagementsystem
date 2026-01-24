abbd0a440743774e397cba8d5d5caa8ff903a8e9
Gab <gabionian7@gmail.com>
2026-01-24 19:53:26 +0800

    9th commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base:"/TailAdmin",
+    base:"/TailAdmin/",
   plugins: [
     react(),
     svgr({
