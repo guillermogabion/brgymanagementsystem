51ed7211bdc3b9c29043d48a9b9d748d5a4d31d0
Gab <gabionian7@gmail.com>
2026-01-25 00:37:23 +0800

    client9

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:5000/api',
-  baseURL: 'http://brgymanagementsystem-api/api',
+  baseURL: 'https://brgymanagementsystem-api.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
