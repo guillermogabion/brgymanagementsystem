72c95371a16fab34ac9cb1a19457ba4918a8e8f0
Gab <gabionian7@gmail.com>
2026-01-24 23:39:13 +0800

    client3

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "https://brgymanagementsystem.vercel.app/"
+}));
 // app.use(express.json()); // Essential for CRUD to read JSON body
 
 app.use(express.json({ limit: '50mb' }));
