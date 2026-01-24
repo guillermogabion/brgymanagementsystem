b8615b85852861650f6cee33a5970ecb92ec386a
Gab <gabionian7@gmail.com>
2026-01-25 01:19:21 +0800

    client17

--- server/src/index.js
+++ server/src/index.js
@@ -6,21 +6,13 @@ require('dotenv').config();
 const app = express();
 const frontendUrl = 'https://brgymanagementsystem-eta.vercel.app';
 
+// 1. Use the middleware (remove the manual app.options block below this)
 app.use(cors({
   origin: frontendUrl,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
-// Handle preflight OPTIONS
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', frontendUrl);
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.sendStatus(204);
-});
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
